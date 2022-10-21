631683daa6f955c337b1fad4d3cc5b4ca0b4f49f
Jisu Park <wltn8223@gmail.com>
2022-10-21 14:42:33 +0900

    4 번째 커밋

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # translate_api
-gunicorn 은 외부서버 데이터베이스를 사용안하고 내부 사용하기때문에 필요없음
--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
