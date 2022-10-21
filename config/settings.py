a74150ab1e127bea9b66ac4d3b738c17d6293aa1
Jisu Park <wltn8223@gmail.com>
2022-10-21 15:02:53 +0900

    5 번째 커밋

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,8 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# STATIC_ROOT = BASE_DIR / "static"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
 # Default primary key field type
